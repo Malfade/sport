9d5ce0a0b4a06a6c227628a78a3f7dcd2e1fc85f
Malfade <nikitaundusk@Gmail.com>
2025-04-13 15:15:00 +0600

    V1.2

--- SportBackend/settings.py
+++ SportBackend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4!@#%$^&*()_+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['kg-sport-tur.onrender.com']  # Для разработки, в продакшене нужно указать конкретные хосты
 
--- SportBackend/urls.py
+++ SportBackend/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('participants/', include('participants.urls')),
+    path('', include('SportBackend.main.urls')),
+    path('participants/', include('SportBackend.participants.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
